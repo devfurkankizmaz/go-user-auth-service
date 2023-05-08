fe9ad23249ceb736be270dd43638ec79a4417834
devfurkankizmaz <dev.furkan@outlook.com>
2023-05-08 15:25:45 +0300

    email validation feature

--- utils/email_validation.go
+++ utils/email_validation.go
@@ -1,1 +1,15 @@
 package utils
+
+import (
+	"errors"
+
+	"github.com/asaskevich/govalidator"
+)
+
+func EmailValid(mail string) error {
+	err := errors.New("wrong email format")
+	if !govalidator.IsEmail(mail) {
+		return err
+	}
+	return nil
+}
--- utils/verify_password.go
+++ utils/verify_password.go
@@ -4,9 +4,8 @@ import (
 	"errors"
 )
 
-var err = errors.New("passwords did not match")
-
 func VerifyPassword(password string, confirmpass string) error {
+	err := errors.New("passwords did not match")
 	if password != confirmpass {
 		return err
 	}
